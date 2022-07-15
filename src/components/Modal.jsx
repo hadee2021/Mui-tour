6c0c5428676af40424f64f23d2dd95755a347c46
hadee2021 <tpghks9920@naver.com>
2022-07-15 18:02:31 +0900

    save

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -3,6 +3,7 @@ import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
+import StaticDateRangePickerDemo from './DatePicker';
 
 const style = {
   position: 'absolute',
@@ -31,12 +32,7 @@ export default function BasicModal() {
         aria-describedby="modal-modal-description"
       >
         <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Text in a modal
-          </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </Typography>
+          <StaticDateRangePickerDemo />
         </Box>
       </Modal>
     </div>
